482809deb043a7bd21599e3f50d06d3c7f8d2561
IvelisBecker <ivelisbecker@gmail.com>
2025-06-15 08:43:35 -0500

    Start to design: Title and background video

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -11,13 +11,16 @@ const Header = () => {
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
-          </h1>
+        <h3 className="m-0 text-9xl text-white tracking-tighter text-balance">
+          Fullstack Developer
+        </h3>
+          <p className="m-0" style={{ 
+            fontSize: '7rem', 
+            
+            }}>
+            Ivelis Becker
+          </p>
         </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
-        </p>
         <div>
           {Auth.loggedIn() ? (
             <>
