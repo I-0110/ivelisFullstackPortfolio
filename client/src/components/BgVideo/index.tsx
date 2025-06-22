b1018cb8a5923915522a8027d867d63df1b504c3
IvelisBecker <ivelisbecker@gmail.com>
2025-06-22 07:34:34 -0500

    It blends my name and customer word when user login

--- client/src/components/BgVideo/index.tsx
+++ client/src/components/BgVideo/index.tsx
@@ -1,12 +1,23 @@
+import Auth from '../../utils/auth';
+// import { type MouseEvent} from 'react';
+
 function Video() {
+    let username = "";
+
+        if (Auth.loggedIn()) {
+            const user = Auth.getUser();
+            console.log("User object:", user);
+            username = user?.data?.name || user?.name ||  "customer";
+        }
+
     return (
         <div className="video-body video-section">
-            <video src="/bgLight-black.mp4" loop autoPlay muted></video>
+            <video src="/bgDark-white.mp4" loop autoPlay muted></video>
             <div className="video-copy">
-                <h1>IVELIS BECKER</h1>
+                <h1>{Auth.loggedIn() ? username : "IVELIS BECKER"}</h1>
             </div>
         </div>
     )
-}
+};
 
 export default Video;
