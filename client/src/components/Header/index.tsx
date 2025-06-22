50aee969450d298108dfea54bafba17744d8f961
IvelisBecker <ivelisbecker@gmail.com>
2025-06-22 05:29:21 -0500

    Start to work on header font

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -9,39 +9,43 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <h3>Hello, my name is</h3>
-        <Link className="spacer text-dark" to="/">
-          <Video />
-        </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Showcasting my work and projects.
-        </p>
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/projects">
-                Ivelis' Projects
-              </Link>
-              <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/me">
-                My Projects
-              </Link>
-              <button className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/projects">
-                Projects
-              </Link>
-              <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/signup">
-                Hire Me
-              </Link>
-            </>
-          )}
-        </div>
+    <header className="flex flex-row justify-center">
+      <div className="text-center">
+      {Auth.loggedIn() ? (
+        <>
+          <h3>Welcome back,</h3>
+          <h3><data value="name"></data></h3>
+          <Link to="/">
+            <Video />
+          </Link>
+          <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/projects">
+            Ivelis' Projects
+          </Link>
+          <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/me">
+            My Projects
+          </Link>
+          <button className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" onClick={logout}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h3>Hello, my name is</h3>
+          <h3>Ivelis</h3>
+          <Link to="/">
+            <Video />
+          </Link>
+          <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+            Showcasting my work and projects.
+          </p>
+          <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/projects">
+            Projects
+          </Link>
+          <Link className="btn btn-lg m-2 bg-light-headline text-light-muted dark:bg-dark-headline dark:text-dark-muted hover:opacity-90 transition" to="/signup">
+            Hire Me
+          </Link>
+        </>
+      )}
       </div>
     </header>
   );
