d52938ef8891ca88ed583ff1e32c8505c76cc8a5
IvelisBecker <ivelisbecker@gmail.com>
2025-06-24 08:52:42 -0500

    Update Resume

--- client/src/pages/Resume.tsx
+++ client/src/pages/Resume.tsx
@@ -1,34 +1,48 @@
 import Header from "../components/Header";
+import { useQuery } from '@apollo/client';
+import { QUERY_USERS } from '../utils/queries';
 
 const Resume = () => {
+    const { loading } = useQuery(QUERY_USERS);
+
     const RESUME_URL = "https://docs.google.com/document/d/e/2PACX-1vTGQphl364LlMKbbjX3DZsxCwrjA5--3cOOipBstFhp9Ulx84nEHkwszSPFCkggInkH9b0z3okkxkux/pub?embedded=true";
 
     return (
         <section className="max-w-5xl mx-auto p-6">
-            <h2>Resume</h2>
-            <Header />
-            <div>
-                <iframe
-                src={RESUME_URL}
-                className="w-full h-[600px]"
-                title="Ivelis Resume"
-                />
+            <h2 className='flex flex-col items-center'>Resume</h2>
+
+            <div className="flex-row justify-center">
+                <div className="col-12 col-md-10 my-3">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                    <Header />
+                    <div>
+                        <iframe
+                        src={RESUME_URL}
+                        className="w-full h-[600px]"
+                        title="Ivelis Resume"
+                        />
+                    </div>
+                    {/* Resume */}
+                    <a
+                        href='https://docs.google.com/document/d/1OCky6jv6FhjHI1q5MDKZ5xHwvVFgzIIYu_NXd2HO6Yo/edit?usp=sharing'
+                        target="_blank"
+                        rel='noopener noreferrer'
+                        className='flex flex-col items-center text-white mt-4'>
+                        Get my resume here:
+                        <img 
+                            src='resume.svg'
+                            alt='Download Resume'
+                            className='icon bg-white'
+                        /> 
+                    </a>
+                </>
+            )} 
             </div>
-            {/* Resume */}
-            <a
-                href='https://docs.google.com/document/d/1OCky6jv6FhjHI1q5MDKZ5xHwvVFgzIIYu_NXd2HO6Yo/edit?usp=sharing'
-                target="_blank"
-                rel='noopener noreferrer'
-                className='flex flex-col items-center text-white mt-4'>
-                Get my resume here:
-                <img 
-                    src='resume.svg'
-                    alt='Download Resume'
-                    className='icon bg-white'
-                /> 
-            </a>
-        </section>
-    );
-};
+        </div>
+    </section>
+)};
 
 export default Resume;
