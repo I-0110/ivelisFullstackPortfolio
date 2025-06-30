6e4a42d3c49559175ec905414e9d293eaa1d4daa
IvelisBecker <ivelisbecker@gmail.com>
2025-06-30 06:21:02 -0500

    Troubleshooting on Header, Project's button fixed

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
           <p className="m-0 bg-dark-headline text-dark-muted hover:opacity-90 transition" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
             Showcasting my work and projects.
           </p>
-          <Link className="btn btn-lg m-2 bg-dark-headline text-dark-muted hover:opacity-90 transition" to="/Home">
+          <Link className="btn btn-lg m-2 bg-dark-headline text-dark-muted hover:opacity-90 transition" to="/projects">
             Projects
           </Link>
           <Link className="btn btn-lg m-2 bg-dark-headline text-dark-muted hover:opacity-90 transition" to="/contact">
